1731194e7a4ad99c3e9b6cb67daf1db52a231faf
Komatsunabok <qron3966@gmail.com>
2025-03-02 20:54:51 +0900

    created image and made the map viewable

--- image.py
+++ image.py
@@ -3,10 +3,9 @@ import tkinter
 def initialize_image():
     global img_bg, img, width, height
     img_bg = [
-        tkinter.PhotoImage(file="image_penpen/chip00.png"),
-        tkinter.PhotoImage(file="image_penpen/chip01.png"),
-        tkinter.PhotoImage(file="image_penpen/chip02.png"),
-        tkinter.PhotoImage(file="image_penpen/chip03.png")
+        tkinter.PhotoImage(file="image/map/map01.png"),
+        tkinter.PhotoImage(file="image/map/map02.png"),
+        tkinter.PhotoImage(file="image/map/map03.png")
     ]
 
     img = img_bg[0]
--- main.py
+++ main.py
@@ -3,12 +3,21 @@ import map
 import key
 import image
 
+DIR_UP = 0
+DIR_DOWN = 1
+DIR_LEFT = 2
+DIR_RIGHT = 3
+
+
 def draw_screen():
+    canvas.delete("SCREEN")
     for y in range(map.map_sizs_y):
         for x in range(map.map_size_x):
             canvas.create_image(x*image.width+image.width/2, 
                                 y*image.height+image.height/2, 
-                                image = image.img_bg[map.map_data[y][x]])
+                                image = image.img_bg[map.map_data[y][x]],
+                                tag = "SCREEN")
+            canvas.create_image()
 
 root = tkinter.Tk()
 root.title("LIFE IS MELTY")
